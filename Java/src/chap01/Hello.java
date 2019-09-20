e53165602f2687154999a22e005e75c1c466e023
기세환 <@222.105.244.162>
2019-09-21 05:35:36 +0900

    java study 20190921

---
+++ Java/src/chap01/Hello.java
@@ -0,0 +1,12 @@
+package chap01;
+/*
+ * 작성자 : James Ki
+ * 작성일 : 2019.09.21
+ */
+public class Hello {
+	//프로그램 실행 진입점
+	public static void main(String[] args) {
+		//콘솔에 출력하는 실행문
+		System.out.println("Hello, wellcome to the java world!");
+	} //end of main
+} //end of class
